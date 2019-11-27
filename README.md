11c2c0314fb6eb73888c0c7af5c0ae4f6ebfe0ed
李锐 <rayestonal@gmail.com>
2019-11-27 17:16:18 +0800

    可重入锁 tryLock

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Java 并发
 > 记录 java 并发的一些示例
 
-## java.util.concurrent 包下的主要类
-> Atomic* 类通过关键字 volatile 和 CAS 算法实现操作的原子性
-> CAS 算法都是在 unsafe 类中实现，该类调用 native 方法
-> 通过反射以获取没有定义 getter 方法的变量 value 的值
+# java.util.concurrent 包下的主要类
+1. Atomic* 类通过关键字 volatile 和 CAS 算法实现操作的原子性
+2. CAS 算法都是在 unsafe 类中实现，该类调用 native 方法
+3. 通过反射以获取没有定义 getter 方法的变量 value 的值
 
+## Atomic*
 ### AtomicBoolean
 > 将 Boolean 类型的 true 和 false 转换成 Integer 类型的 1 和 0
 
@@ -17,3 +18,12 @@
 
 ### AtomicIntegerArray
 > 针对数组中的每个元素进行原子操作，与上述类似
+
+
+## lock
+### LockSupport
+1. LockSupport 类可以阻塞当前线程以及唤醒指定被阻塞的线程
+2. 主要是通过 park() 和 unpark(thread) 方法来实现
+3. 每个线程都有一个许可(permit)，permit 只有两个值 1 和 0，默认是 0
+
+
--- src/main/java/mobi/concurrent/lock/ReentrantLockTryLockTest.java
+++ src/main/java/mobi/concurrent/lock/ReentrantLockTryLockTest.java
@@ -2,38 +2,38 @@ package mobi.concurrent.lock;
 
 import mobi.common.Note;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 @Note("ReentrantLock")
-public class ReentrantLockTest implements Runnable {
+public class ReentrantLockTryLockTest implements Runnable {
 
     private static ReentrantLock lock = new ReentrantLock(true);
 
-    private static int num = 0;
-
-    public static void main(String[] args) throws InterruptedException {
-        Thread t1 = new Thread(new ReentrantLockTest(), "t1");
-        Thread t2 = new Thread(new ReentrantLockTest(), "t2");
-        t1.start();
-        t2.start();
-        t1.join();
-        t2.join();
-        System.out.println(num);
-    }
-
     @Override
     public void run() {
-
-        for (int i = 0; i < 100000; i++) {
-            lock.tryLock();
-            try {
-                System.out.println(Thread.currentThread().getName() + ": " + num);
-                num++;
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
+        try {
+            if (lock.tryLock(5, TimeUnit.SECONDS)) {
+                System.out.println(Thread.currentThread().getName() + ": 获取到了当前锁");
+                Thread.sleep(6000);
+            } else {
+                System.out.println(Thread.currentThread().getName() + ": 获取锁失败");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            if (lock.isHeldByCurrentThread()) {
+                System.out.println(Thread.currentThread().getName() + ": 持有当前锁，可以释放");
                 lock.unlock();
             }
         }
     }
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new ReentrantLockTryLockTest(), "t1");
+        Thread t2 = new Thread(new ReentrantLockTryLockTest(), "t2");
+        t1.start();
+        t2.start();
+    }
+
 }
