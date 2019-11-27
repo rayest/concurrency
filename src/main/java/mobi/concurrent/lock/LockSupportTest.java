ba808b405b0815c5d9c26f259b5b2f452ffa336d
李锐 <rayestonal@gmail.com>
2019-11-27 17:15:47 +0800

    可重入锁 lock

---
+++ src/main/java/mobi/concurrent/lock/LockSupportTest.java
@@ -0,0 +1,26 @@
+package mobi.concurrent.lock;
+
+import mobi.common.Note;
+
+import java.util.concurrent.locks.LockSupport;
+
+@Note("LockSupport")
+public class LockSupportTest {
+    public static void main(String[] args) throws InterruptedException {
+        testUnpark();
+    }
+
+    @Note("park: 主线程被阻塞，许可设为 0")
+    private static void testPark() {
+        System.out.println("开始");
+        LockSupport.park();
+        System.out.println("被主线程阻塞而不得执行 Blocking...");
+    }
+
+    @Note("unpark: 当许可恢复为 1 时，主线程继续执行")
+    private static void testUnpark() {
+        System.out.println("开始");
+        LockSupport.unpark(Thread.currentThread());
+        System.out.println("继续执行");
+    }
+}
---
+++ src/main/java/mobi/concurrent/lock/ReentrantLockTryLockTest.java
@@ -0,0 +1,39 @@
+package mobi.concurrent.lock;
+
+import mobi.common.Note;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+@Note("ReentrantLock")
+public class ReentrantLockTest implements Runnable {
+
+    private static ReentrantLock lock = new ReentrantLock(true);
+
+    private static int num = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(new ReentrantLockTest(), "t1");
+        Thread t2 = new Thread(new ReentrantLockTest(), "t2");
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(num);
+    }
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i < 100000; i++) {
+            lock.tryLock();
+            try {
+                System.out.println(Thread.currentThread().getName() + ": " + num);
+                num++;
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+}
