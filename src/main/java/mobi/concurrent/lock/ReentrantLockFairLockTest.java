a7091a95a7a0e0929b888b95f40eef49942088d7
李锐 <rayestonal@gmail.com>
2019-11-27 17:36:18 +0800

    可重入锁 公平锁

---
+++ src/main/java/mobi/concurrent/lock/ReentrantLockFairLockTest.java
@@ -0,0 +1,31 @@
+package mobi.concurrent.lock;
+
+import mobi.common.Note;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+@Note("ReentrantLock 的公平锁测试")
+public class ReentrantLockFairLockTest {
+
+    private static ReentrantLock lock = new ReentrantLock(true);
+
+    private static void testFairLock() {
+        lock.lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + ": 获得了锁");
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 50; i++) {
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName() + ": 启动");
+                ReentrantLockFairLockTest.testFairLock();
+            }).start();
+        }
+    }
+
+}
